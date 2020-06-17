894cbc5565006505570f696a36169426b7f8bdaf
mohamed Elesaily <36012836+Mohamed-Elesaily@users.noreply.github.com>
2020-06-17 21:52:49 +0200

    Update main.cpp
    test database

--- main.cpp
+++ main.cpp
@@ -10,18 +10,12 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    DB* sql = sql->getInstance();
+    DB* b= b->getInstance();
+    b->setpath("db.sqlite");
+    b->deletefile(1);
+    qDebug()<< b->getFile(5);
 
-//      ChooseDir b;
-//      b.choosePath("/home/msm/Qt/build-ds-Desktop-Debug/questions");
-//     qDebug()<< b.buildData();
-//     DB* sql = sql->getInstance();
-
-//DB* b= b->getInstance();
-//b->setpath("db.sqlite");
-//b->deletefile(1);
-//qDebug()<< b->getFile(5);
-string_manip h;
-h.sendToTrie();
    return a.exec();
 }
 
